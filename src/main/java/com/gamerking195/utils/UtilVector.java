99ec30d6e3459aefa26336f680d31722afe57721
GamerKing195 <gamerking195@gmail.com>
2016-06-16 11:58:30 -0600

    Added UtilVector

---
+++ src/main/java/com/gamerking195/utils/UtilVector.java
@@ -0,0 +1,22 @@
+package com.gamerking195.utils;
+
+import org.bukkit.Location;
+import org.bukkit.util.Vector;
+
+public class UtilVector 
+{
+	/**
+	 * 
+	 * Turns a speed into a vector.
+	 * 
+	 * @param loc The location from which the vector will be calculated.
+	 * @param speed The speed in meters per second.
+	 * @return The vector with the speed amount
+	 */
+	public static Vector getSpeedAsVector(Location loc, double speed)
+	{
+		Vector direction = loc.getDirection();
+		Vector velocity = direction.multiply(speed/50);
+		return velocity;
+	}
+}
